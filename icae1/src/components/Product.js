c696bbfeeff653296c8aab54facb91ae1eec2859
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-12-03 16:12:53 +0000

    uploaded the image

--- icae1/src/components/Product.js
+++ icae1/src/components/Product.js
@@ -15,14 +15,14 @@ export default function Product(){
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                <img src={requestAnimationFrame('../assets/image'+flower.img)} alt={flower.name} />
                 <div class="card-body">
                     <h5 class="card-title">Price:</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
                         <input type="number" id="quantity" name="quantity" />
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={handleAddtoCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
